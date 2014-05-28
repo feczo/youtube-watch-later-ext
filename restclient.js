1596650202c0ebbdc907373fc2fa96ca51a9757a
Szabolcs Feczak <szabolcs.feczak@gmail.com>
2014-05-28 10:13:27 +1000

    fixing incorrect version set

--- restclient.js
+++ restclient.js
@@ -1,62 +1,54 @@
-  window.gapi = {};
-  window.gapi.auth = {};
-  window.gapi.client = {};
+window.gapi = {};
+window.gapi.auth = {};
+window.gapi.client = {};
 
-  gapi.client.request = function (args) {
-    if (typeof args !== 'object')
-      throw new Error('args required');
-    if (typeof args.callback !== 'function')
-      throw new Error('callback required');
-    if (typeof args.path !== 'string')
-      throw new Error('path required');
+
+gapi.client.request = function (args, retry) {
+    retry = typeof retry !== 'undefined' ? retry : true;
+    if (typeof args !== 'object') throw new Error('args required');
+    if (typeof args.callback !== 'function') throw new Error('callback required');
+    if (typeof args.path !== 'string') throw new Error('path required');
 
     if (args.root && args.root === 'string') {
-      var path = args.root + args.path;
+        var path = args.root + args.path;
     } else {
-      var path = 'https://www.googleapis.com' + args.path;
+        var path = 'https://www.googleapis.com' + args.path;
     }
 
     if (typeof args.params === 'object') {
-      var deliminator = '?';
-      for (var i in args.params) {
-        path += deliminator + encodeURIComponent(i) + "="
-          + encodeURIComponent(args.params[i]);
-        deliminator = '&';
-      }
+        var deliminator = '?';
+        for (var i in args.params) {
+            path += deliminator + encodeURIComponent(i) + "=" + encodeURIComponent(args.params[i]);
+            deliminator = '&';
+        }
     }
 
     var xhr = new XMLHttpRequest();
     xhr.open(args.method || 'GET', path);
     xhr.setRequestHeader('Authorization', 'Bearer ' + args.token);
     if (typeof args.body !== 'undefined') {
-      xhr.setRequestHeader('content-type', 'application/json');
-      xhr.send(JSON.stringify(args.body));
+        xhr.setRequestHeader('content-type', 'application/json');
+        xhr.send(JSON.stringify(args.body));
     } else {
-      xhr.send();
+        xhr.send();
     }
 
     xhr.onerror = function () {
-      // TODO, error handling.
-      debugger;
+        // TODO, error handling.
+        debugger;
     };
 
-    xhr.onload = function() {
-      var rawResponseObject = {
-        // TODO: body, headers.
-        gapiRequest: {
-          data: {
-            status: this.status,
-            statusText: this.statusText
-          }
+    xhr.onload = function () {
+        if (this.status === 401 && retry) {
+            chrome.identity.removeCachedAuthToken({
+                'token': args.token
+            }, gapi.client.request(args, false));
         }
-      };
-
-      var rawResp = JSON.stringify(rawResponseObject);
-      if (this.response) {
-        var jsonResp = JSON.parse(this.response);
-        args.callback(jsonResp, rawResp);
-      } else {
-        args.callback(null, rawResp);
-      }
+        if (this.response) {
+            var jsonResp = JSON.parse(this.response);
+            args.callback(jsonResp, this.status);
+        } else {
+            args.callback(null, rawResp);
+        }
     };
-  };
+};
